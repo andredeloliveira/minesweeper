cdde0070951ed6e7b5b2f03eed53a1eb1ed23473
Andre de Oliveira <oliveira.andrede@gmail.com>
2024-10-02 21:35:41 -0300

    Add handler function, getting the element to update
    - Added handler function on cell container
    - Added "use client" on page, to accept functions to be passed as
    - props (this took a while)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 type CellValue = number | 'bomb'
 
 type CellState = "flagged" | "hidden" | "revealed"
@@ -12,12 +13,21 @@ const MineSweeper = () => {
   const columns: number = 10
   const cells: Cell[][] = generateBoard(rows, columns)
 
+
+
   function generateBoard(rows: number, columns: number): Cell[][] {
     return Array(rows).fill(null).map(() => {
       return Array(columns).fill(null).map(() => {
         return { state: "hidden", value: Math.random() > 0.8 ? "bomb" : 0}
       })
     })
+  }
+
+
+  // considers the position of the cell, to guarantee it will be updated
+  function handleOnClick(i: number, j: number) {
+    console.log("clicked")
+    console.log(cells[i][j])
   }
 
   return (
@@ -27,8 +37,8 @@ const MineSweeper = () => {
           <div key={i} className="row">
             { row.map((col, j) => {
                 return (
-                  <div key={i+j} className="col">
-                    <Cell state={col.state} value={col.value} />
+                  <div key={i+j} className="col" onClick={() => handleOnClick(i, j)}>
+                    <CellComponent state={col.state} value={col.value} />
                   </div>
                 )
             }) }
@@ -39,10 +49,10 @@ const MineSweeper = () => {
   )
 }
 
-function Cell({ value, state }: Cell) {
+function CellComponent(props: Cell) {
   return (
-    <div className="cell">
-      <span>{value}</span>
+    <div className={`cell`}>
+      <span>{props.value}</span>
     </div>
   )
 }
